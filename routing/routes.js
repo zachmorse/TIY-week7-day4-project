90343439f14733343c570cfff8e06121855837c4
Zach Morse <zacharymorse@gmail.com>
2017-07-06 15:43:38 -0500

    routing complete, beginning work on page integration

--- routing/routes.js
+++ routing/routes.js
@@ -8,13 +8,33 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/addentry", (req, res) => {
+router.post("/addcar", (req, res) => {
   var carData = req.body;
   let newCarEntry = new Car(carData);
   newCarEntry
     .save()
     .then(savedCar => {
       res.send(savedCar);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+router.put("/updatecar/:id", (req, res) => {
+  Car.updateOne({ _id: req.params.id }, req.body)
+    .then(updatedCar => {
+      res.send(updatedCar);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+router.delete("/deletecar/:id", (req, res) => {
+  Car.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.send(`entry #${req.params.id} deleted from database`);
     })
     .catch(err => {
       res.status(500).send(err);
